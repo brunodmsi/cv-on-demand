3de933daa9fa2eee73150703b6411214bcb5234a
Bruno De Masi <brunodemasi1@gmail.com>
2023-03-24 00:45:36 -0300

    feat: first latex document generated -- only text tho

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,63 @@
-console.log('Hello world');
+type Template = {
+	latex: string;
+	data: Record<string, any>;
+};
+
+const firstTemplate: Template = {
+	latex: `
+		Hello {{name}}
+	`,
+	data: {
+		name: 'Bruno De Masi',
+		currentJob: 'Fullstack Developer',
+	},
+};
+
+const templates = [firstTemplate, { latex: '', data: {} }];
+
+const document = `
+	\\documentclass{article}
+	\\usepackage{graphicx} % Required for inserting images
+
+	\\begin{document}
+
+	{{children}}
+
+	\\end{document}
+`;
+
+const hydrate = (texTemplate: string, data: Record<string, any>): string => {
+	let hydratable = texTemplate;
+
+	const templateVariables = texTemplate.match(/{{[a-zA-Z]\w+(.*?)}}/g) ?? [];
+
+	for (const texVariable of templateVariables) {
+		const variable = texVariable.slice(2, texVariable.length - 2); // remove {{}}
+
+		const variableData = data[variable];
+		if (!variableData) {
+			hydratable = hydratable.replace(texVariable, '----');
+		} else {
+			hydratable = hydratable.replace(texVariable, variableData);
+		}
+	}
+
+	return hydratable;
+};
+
+let body = '';
+
+for (let i = 0; i < templates.length; i++) {
+	const hydrated = hydrate(templates[i].latex, templates[i].data);
+
+	body = `
+		${body}
+		${hydrated}
+	`;
+}
+
+const documentHydrated = hydrate(document, {
+	children: body,
+});
+
+console.log(documentHydrated);
